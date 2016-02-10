c30d8422f5519c3f05709db100693bcc5c377a98
ryan-schurton <ryan.schurton@gmail.com>
2016-02-10 17:08:07 -0500

    goodluck

--- app/app.js
+++ app/app.js
@@ -1,19 +1,25 @@
 'use strict';
 
-var app = angular.module('shopApp',['ui.router',]);
+var app = angular.module('shopApp',['ui.router']);
 
 app.config(function($stateProvider, $httpProvider,$urlRouterProvider){
-	
-	$urlRouterProvider.otherwise('/');
+  
+  $urlRouterProvider.otherwise('/');
 
-	$stateProvider
-	.state('shop',{
-		url:'/',
-		templateUrl:'',
-		controller:'',
-	});
+  $stateProvider
+  .state('shop',{
+    url:'/',
+    templateUrl:'site/partials/home.html',
+    controller:'songControl as ctrl',
+  })
+  .state('admin',{
+    url:'/admin',
+    templateUrl:'site/partials/product.html',
+    controller:'AdminCtrl as ctrl',
+    //TODO #2 Resolve Products before admin page load
+  });
 
-	$httpProvider.interceptors.push(function(){
+  $httpProvider.interceptors.push(function(){
        return {
            request: function(config) {
                return config;
@@ -21,10 +27,50 @@ app.config(function($stateProvider, $httpProvider,$urlRouterProvider){
            response: function(response) {
                var auth_token = response.headers('authentication');
                if(localStorage.authToken == undefined && auth_token != null){
-               	localStorage.authToken = auth_token;
+                localStorage.authToken = auth_token;
                }
                return response;
            }
        }
    });
 });
+
+	//$urlRouterProvider.otherwise('/');
+
+	// $stateProvider
+	// .state('shop',{
+	// 	url:'/',
+	// 	templateUrl:'',
+	// 	controller:'',
+	// });
+
+  //   $routeProvider
+  //   .when('/',{
+  //     templateUrl:'index.html',
+  //     controller:'songControl as ctrl'
+  //   })
+  //   .when('/movie/:movieId',{
+  //     templateUrl:'movieDetails.html',
+  //     controller:'MovieDetailsCtrl as ctrl'
+  //   })
+  //   .otherwise({
+  //     redirectTo:'/'
+  //   });
+  // })
+
+// 	$httpProvider.interceptors.push(function(){
+//        return {
+//            request: function(config) {
+//                return config;
+//            },
+//            response: function(response) {
+//                var auth_token = response.headers('authentication');
+//                if(localStorage.authToken == undefined && auth_token != null){
+//                	localStorage.authToken = auth_token;
+//                }
+//                return response;
+//            }
+//        }
+//    });
+// });
+
